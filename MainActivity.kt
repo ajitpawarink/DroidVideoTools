e7b4aefe5b90d276cd1bbd5d441b48faef7d42e5
Aj <ajit@tagrem.com>
2024-02-27 16:57:15 +0530

    commit message

---
+++ MainActivity.kt
@@ -0,0 +1,15 @@
+// MainActivity.kt
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Set the text of the TextView to "Welcome" when the activity is created
+        textViewMessage.text = "Welcome"
+    }
+}
